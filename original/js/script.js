1251cc7ac2eedac873a2c1e093f9945012e39ae0
MinakoTokimatsu <tokiminalife@yahoo.co.jp>
2017-05-02 16:21:54 +0900

    parallax.min add
    parallax.min add

--- original/js/script.js
+++ original/js/script.js
@@ -1,11 +1,7 @@
-//Skipprの初期化
-$("document").ready(function() {
-    $("#top").skippr();
-});
 // オプションを指定してSkipprの実行
-$("#top").skippr({
+$("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
-    transition : 'slide',
+    transition : 'fade',
     // 変化に係る時間(ミリ秒)
     speed : 1000,
     // easingの種類
